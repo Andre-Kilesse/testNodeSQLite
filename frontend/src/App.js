e5df98f55a2bd54109b7142b74eaead0153708ce
Andre Kilesse <kilesseandre@gmail.com>
2025-03-15 13:04:08 -0300

    Reorganiza estrutura do repositório, movendo backend e frontend para pastas separadas.

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import MenuSuperior from "./components/MenuSuperior";
+import InclusaoLivros from "./components/InclusaoLivros";
+
+function App() {
+  return (
+    <>
+      <MenuSuperior/>
+      <InclusaoLivros/>
+    </>
+  );
+}
+
+export default App;
